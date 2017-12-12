0bde08257ce694b24943935f2ee400f4d235af4a
Chakrit Wichian <service@chakrit.net>
2017-12-12 20:32:56 +0700

    Initial version.

--- sync.rb
+++ sync.rb
@@ -1,11 +1,18 @@
-# SYNC
+# frozen_string_literal: true
 
+# Topics.
 module Synchronization
   def cas; end
+
   def memory_barrier; end
+
   def semaphore; end
+
   def mutex; end
+
   def monitor; end
+
   def promise; end
+
   def csp; end
 end
